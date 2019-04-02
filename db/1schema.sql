e54d86de3496e8dc80b5d6fe4a659bbd0645b825
liangcan <liangcan.jxjy.edu@gmail.com>
2019-04-02 15:41:31 +0800

    添加Dockerfile

---
+++ db/1schema.sql
@@ -0,0 +1,8 @@
+-- martin 核心表
+create database `martin` default character set utf8mb4 collate utf8mb4_general_ci;
+
+-- martin 工作流相关库
+create database `martin_ac` default character set utf8mb4 collate utf8mb4_general_ci;
+
+-- martin zipkin相关库
+create database `martin_zipkin` default character set utf8mb4 collate utf8mb4_general_ci;
