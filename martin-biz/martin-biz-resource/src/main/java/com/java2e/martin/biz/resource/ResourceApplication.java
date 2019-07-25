203a43489d5463a91e67481a601f5a3f8ce0cada
liangcan <liangcan.jxjy.edu@gmail.com>
2019-07-25 14:31:24 +0800

    修正包名不对导致的配置文件无法正确配置

--- martin-biz/martin-biz-resource/src/main/java/com/java2e/martin/biz/resource/ResourceApplication.java
+++ martin-biz/martin-biz-resource/src/main/java/com/java2e/martin/biz/resource/ResourceApplication.java
@@ -1,15 +1,11 @@
 package com.java2e.martin.biz.resource;
 
 import cn.hutool.core.convert.Convert;
-import com.java2e.martin.biz.resource.component.PermitAllUrlPropertiesTest;
 import com.java2e.martin.common.security.annotation.EnableMartinResourceServer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.List;
 
 /**
  * @Author: liangcan
@@ -19,21 +15,9 @@ import java.util.List;
  */
 @SpringBootApplication
 @EnableMartinResourceServer
-public class ResourceApplication implements CommandLineRunner {
-
-
-    @Autowired
-    private PermitAllUrlPropertiesTest permitAllUrlPropertiesTest;
-
+public class ResourceApplication {
     public static void main(String[] args) {
-
         SpringApplication.run(ResourceApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-
-        System.out.println("permitAllUrlProperties.getIgnoreUrls = " + Convert.toStr(permitAllUrlPropertiesTest.getIgnoreUrls()));
-        System.out.println("permitAllUrlPropertiesTest.getScope = " + permitAllUrlPropertiesTest.getScope());
-    }
 }
