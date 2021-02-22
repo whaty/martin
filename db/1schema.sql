c0aacc71a6f3436d493e47718099db3dd96c2aa0
liangcan.jxjy.edu@gmail.com <liangcan.jxjy.edu@gmail.com>
2021-02-22 11:36:54 +0800

    数据库原始sql修改

--- db/1schema.sql
+++ db/1schema.sql
@@ -1,11 +1,5 @@
--- Martin核心表
+-- Martin 核心表
 create database `martin` default character set utf8 collate utf8_general_ci;
 
--- Martin工作流相关库
-create database `martin_ac` default character set utf8 collate utf8_general_ci;
-
--- Martinzipkin相关库
-create database `martin_zipkin` default character set utf8 collate utf8_general_ci;
-
--- Martinzipkin相关库
+-- nacos_config 相关库
 create database `nacos_config` default character set utf8 collate utf8_general_ci;
