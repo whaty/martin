f54e1734b80a6a9d2de0d66d204e15b48b4479af
liangcan.jxjy.edu@gmail.com <liangcan.jxjy.edu@gmail.com>
2021-03-08 10:30:15 +0800

    升级nacos的共享配置，修改Dockerfile

--- db/1schema.sql
+++
@@ -1,5 +0,0 @@
--- Martin 核心表
-create database `martin` default character set utf8 collate utf8_general_ci;
-
--- nacos_config 相关库
-create database `nacos_config` default character set utf8 collate utf8_general_ci;
